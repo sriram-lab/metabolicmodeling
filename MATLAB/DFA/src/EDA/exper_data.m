d76eef002bba6effa1affc7c93077784fa5ba9c9
sgjoshi25 <surabhij@umich.edu>
2020-06-04 12:16:40 -0400

    The .m of the .mlx file updated

--- MATLAB/DFA/src/EDA/exper_data.m
+++ MATLAB/DFA/src/EDA/exper_data.m
@@ -11,15 +11,12 @@ Create the correlation matrix, CV
 Create heatmaps to visually understand the standard deviations and averages. 
 Scatterplots of the mean and standard deviations
 Hisograms of the mean and standard deviations
-
-
-
-Summary
-This script reads in some experimental data and performs exploratory initial statistical analysis to understand how each condition affects the metabolomics before running DFA. 
-% download the experimental data and create two tables, one for each
-% cancer cell line TU8902 and HCT116.
 %}
 
+%% Summary
+%This script reads in some experimental data and performs exploratory initial statistical analysis to understand how each condition affects the metabolomics before running DFA. 
+% download the experimental data and create two tables, one for each
+% cancer cell line TU8902 and HCT116.
 experimental_data_TU8902 = readtable('Barb Nelson Pos Table-20180410 - HL.xlsx', ...
 'Sheet', 'TU8902');
 experimental_data_HCT116 = readtable('Barb Nelson Pos Table-20180410 - HL.xlsx', ... 
@@ -28,35 +25,23 @@ experimental_data_HCT116 = readtable('Barb Nelson Pos Table-20180410 - HL.xlsx',
 experimental_data_TU8902.transition = [];
 experimental_data_HCT116.transition = [];
 
+%download metabolomics and clusterring data
+metabolomics = readtable('Barb Nelson Pos Table-20180410 - HL.xlsx', ... 
+    'Sheet', 'Clustering');
+metabolomics(1,:) = [];
 
-%%Create tables for averages and standard deviations. 
-%%This here is going to take the averages and standard deviations from the original experimental data and fill two new tables with their values. Any NaN will be turned to 0 prior to finding the mean and standard deviations. 
-Tabl_average_TU8902 = experimental_data_TU8902;
-Tabl_std_TU8902 = experimental_data_TU8902;
-Tabl_average_HCT116 = experimental_data_HCT116;
-Tabl_std_HCT116 = experimental_data_HCT116;
+%% Create tables for averages and standard deviations. 
+%This here is going to take the averages and standard deviations from the original experimental data and fill two new tables with their values. Any NaN will be turned to 0 prior to finding the mean and standard deviations. 
+Tabl_average_TU8902 = table_calc(experimental_data_TU8902, 'mean');
+Tabl_average_TU8902(227,:) = [];
+Tabl_std_TU8902 = table_calc(experimental_data_TU8902, 'stddev');
+Tabl_std_TU8902(227,:) = [];
+Tabl_average_HCT116 = table_calc(experimental_data_HCT116, 'mean');
+Tabl_std_HCT116 = table_calc(experimental_data_HCT116, 'stddev');
 
-%fill the tables with averages and standard deviations and then delete the
-%the last two trials for each condition
-for n = 2:13
-    Tabl_average_TU8902(:,(n)) = num2cell(nanmean(Tabl_average_TU8902{:,n:n+2},2));
-    Tabl_std_TU8902(:,(n)) = num2cell(nanstd(Tabl_std_TU8902{:,n:n+2},[ ],2));
-    Tabl_average_HCT116(:,(n)) = num2cell(nanmean(Tabl_average_HCT116{:,n:n+2},2));
-    Tabl_std_HCT116(:,(n)) = num2cell(nanstd(Tabl_std_HCT116{:,n:n+2},[ ],2));
-    
-    Tabl_average_TU8902(:,(n+2)) = [];
-    Tabl_std_TU8902(:,(n+2)) = [];
-    Tabl_average_TU8902(:,(n+1)) = [];
-    Tabl_std_TU8902(:,(n+1)) = [];
-    
-    Tabl_average_HCT116(:,(n+2)) = [];
-    Tabl_std_HCT116(:,(n+2)) = [];
-    Tabl_average_HCT116(:,(n+1)) = [];
-    Tabl_std_HCT116(:,(n+1)) = [];
-end 
 
 %% Create a corelation matrix and a following heatmap
-% Do this by first converting all the NaN values into 0 and then create a corelation matrix of the metabolites using an array of averages.  
+%Do this by first converting all the NaN values into 0 and then create a corelation matrix of the metabolites using an array of averages.  
 % create arrays from the tables above. 
 mean_metabolomic_array_TU8902 = table2array(Tabl_average_TU8902(:,2:end));
 std_metabolomic_array_TU8902 = table2array(Tabl_std_TU8902(:,2:end));
@@ -112,9 +97,9 @@ figure (2);
 scatter(mean_metabolomic_array_TU8902(:,1), std_metabolomic_array_TU8902(:,1));
 
 figure (3);
-metabolite_array = categorical(table2array(Tabl_average_TU8902(:,1)));
-parallelcoords(mean_metabolomic_array_TU8902((1:10),:), 'group', metabolite_array(1:10));
-title('Mean - first 10 metabolites');
+metabolomics = table2array(metabolomics);
+parallelcoords(mean_metabolomic_array_TU8902, 'group', metabolomics(:,2));
+title('Mean of the metabolomics');
 
 figure (4);
 parallelcoords(CV_TU8902((1:10),:), 'group', metabolite_array(1:10));
